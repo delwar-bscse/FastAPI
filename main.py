069eb1a7504b502bc7e498a92719211919e78527
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-05-21 00:40:26 +0600

    PostgreSQL Database connected successfully

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='123456', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successful")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error: ", error)
+        time.sleep(3)
 
 
 # Define request body schema.
@@ -17,7 +31,9 @@ def create_post(post:Course):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    cursor.execute("SELECT * FROM course")
+    data = cursor.fetchall()
+    return {"data": data}
 
 @app.get("/course")
 def read_root():
