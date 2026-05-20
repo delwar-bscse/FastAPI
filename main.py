a29eed2e02da9da075056ca2d544b9eb0dd03ba3
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-05-20 19:05:06 +0600

    handle post request

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
+
+# Define request body schema.
+class Course(BaseModel):
+    name:str
+    instructor:str
+    duration:float
+    website:HttpUrl
+
+@app.post("/post")
+def create_post(post:Course):
+    return post
 
 @app.get("/")
 def read_root():
