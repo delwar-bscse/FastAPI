0d6635ab855702b0462fcf3722b32683c292545e
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-05-21 08:23:41 +0600

    Complete Get, Post, Put, Delete operation with PostgreSQL Database

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status, Response
 from pydantic import BaseModel, HttpUrl
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -6,6 +6,7 @@ import time
 
 app = FastAPI()
 
+# Connect to database
 while True:
     try:
         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='123456', cursor_factory=RealDictCursor)
@@ -25,16 +26,42 @@ class Course(BaseModel):
     duration:float
     website:HttpUrl
 
-@app.post("/post")
-def create_post(post:Course):
-    return post
+# Define routes and functions for the API
+@app.post("/")
+def create_post(body:Course):
+    cursor.execute("""INSERT INTO course (name, instructor, duration, website) VALUES (%s, %s, %s, %s) RETURNING *""", (body.name, body.instructor, body.duration, str(body.website)))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
 
 @app.get("/")
-def read_root():
-    cursor.execute("SELECT * FROM course")
+def get_courses():
+    cursor.execute("SELECT * FROM course ORDER BY id ASC")
     data = cursor.fetchall()
     return {"data": data}
 
-@app.get("/course")
-def read_root():
-    return {"title": "FastAPI"}
+@app.get("/{id}")
+def get_course(id:int):
+    cursor.execute("SELECT * FROM course WHERE id = %s", (str(id),))
+    data = cursor.fetchone()
+    if not data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Course with id {id} not found")
+    return {"data": data}
+
+@app.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    cursor.execute("DELETE FROM course WHERE id = %s RETURNING *", (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if deleted_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Course with id {id} not found")
+    return Response(status_code=status.HTTP_204_NO_CONTENT, content="Course deleted successfully")
+
+@app.put('/{id}')
+def update_post(id:int, body:Course):
+    cursor.execute("UPDATE course SET name = %s, instructor = %s, duration = %s, website = %s WHERE id = %s RETURNING *", (body.name, body.instructor, body.duration, str(body.website), str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Course with id {id} not found")
+    return {"data": updated_post}
